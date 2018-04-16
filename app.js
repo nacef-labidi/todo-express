378d92d5c6c2d5444a1e167107076fba09dade40
Nacef LABIDI <nacef.labidi@gmail.com>
2018-04-16 17:42:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var db_password = process.env.MONGODB_PASSWORD;
 var db_host = process.env.DATABASE_SERVICE_NAME;
 var db_name = process.env.MONGODB_DATABASE;
 var db_url = 'mongodb://' + db_user + ':' + db_password + '@' + db_host + '/' + db_name + '?auto_reconnect';
+console.log(db_url);
 var db = mongoskin.db(db_url, {safe:true});
 var app = express();
 
